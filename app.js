4840bd8271c50a68985a78c9b4c21ed10600352c
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-03-12 22:01:23 -0700

    inquirer installed and functional

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-    console.log('================');
+// const pageHTML = generatePage(name, github);
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-printProfileData(profileDataArgs);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
